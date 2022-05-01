f21944d07bfb4e33f090d90576fc7c7aaa777bc1
maratgareev97 <maratgareev97@gmail.com>
2022-05-01 18:19:28 +0300

    test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,14 +7,14 @@ public class App {
         HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
         HelloWorld ok = (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
-        System.out.println(bean.equals(ok));
+        System.out.println(bean == ok);
 
         Cat cat = (Cat) applicationContext.getBean("cat");
         Cat cat1 = (Cat) applicationContext.getBean("cat");
 //        System.out.println(cat.equals(cat1));
 //        System.out.println(cat.getMessage());
 //        System.out.println(cat.getMessage());
-        System.out.println(cat.equals(cat1));
+        System.out.println(cat == cat1);
 //        if (bean.equals(cat)){
 //            System.out.println(bean.equals(cat));
 //        }
