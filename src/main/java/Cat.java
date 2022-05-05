8458c10fee98f54f6fee90640cb5680fcd7fb211
gareev_mr <maratgareev97@gmail.com>
2022-05-05 08:29:28 +0300

    test1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String message1;
+
+    public String getMessage() {
+        return message1;
+    }
+
+    public void setMessage(String message1) {
+        this.message1 = message1;
+    }
+}
